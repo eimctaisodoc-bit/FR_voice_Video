b1d963357b42ba9119cfb37251e7e85df38a5d1f
eimctaisodoc-bit <eimcta.isodoc@gmail.com>
2026-03-25 13:09:03 +0545

    Update voice.jsx

--- src/assets/component/voice.jsx
+++ src/assets/component/voice.jsx
@@ -2,7 +2,11 @@ import React, { useEffect, useMemo, useRef } from "react"
 import { io } from "socket.io-client";
 // import { URL } from "./chat";
 export const Voice_ = () => {
-    const socket = useMemo(() => io("https://backend-virid-gamma-gtvuhe17sx.vercel.app/"), []);
+    const socket = useMemo(() => io("https://backend-virid-gamma-gtvuhe17sx.vercel.app/",{
+  path: "/socket.io/",
+  transports: ["polling"], 
+  withCredentials: true
+}), []);
     console.log(socket)
 
     useEffect(() => {
